b1ebf02d241ad38934d4b40591532969fc970f19
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-04-25 00:23:09 +0300

    Add a dropdown-based select-like control

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # react-controls
- A collection of (hopefully) reusable React controls
+
+A collection of (hopefully) reusable React controls
+
+## Bootstrap-based controls
+
+### 1. Select Dropdown
+
+A control that mimics the ordinary `<select>`, but in an more cross-browser look-n-feel.
+It is built on Bootstrap's (`react-bootstrap`) dropdown button/menu funtionality.
+
+#### Properties - Select Dropdown:
+
+| Name | Required | Type | Description | Example |
+| ---- | -------- | ---- | ----------- | --------|
+| `id` | Yes | `String` | The DOM id of the dropdown element| `select-something` |
+| `name` | No | `String` | The DOM name of the controlled input element (if given)| `something` |
+| `width` | No | `Number`, `String` | The width of the (toggle) button| `4em` |
+| `value` | No | `String` | The current value of this input (aka selected option) | `foo` |
+| `placeholder` | No | `String` | The placeholder text to be shown when value is `null` | `select...` |
+| `onChange` | Yes | `(val) => ()` | A callback to be invoked when input has changed|  |
+| `onSelect` | No | `(val) => ()` | A callback to be invoked when an option was selected|  |
+
+Notes:
+
+- the `onChange` callback does not accept a synthetic event as 1st argument (as in `<select>`),
+  but the changed value instead.
+- the `onSelect` callback (does not exist in `<select>`) is invoked regardless of if the selected 
+  value has changed. 
+
+#### Example - Select Dropdown:
+
+````javascript
+var Select = require('react-controls/components/select-dropdown');
+...
+var Foo = React.createClass({
+  ...
+  // Assume Foo has the following props: timespan, setTimespan
+  render: function () {
+    return (
+      <Select
+        id={'select-timespan'}
+        name={'timespan'}
+        value={this.props.timespan}
+        onChange={(val) => (this.props.setTimespan(val))}
+       >
+        <option key={'day'} value={'day'}>{'Day'}</option>
+        <option key={'month'} value={'month'}>{'Month'}</option>
+        <option key={'year'} value={'year'}>{'Year'}</option>
+      </Select>
+    );
+});
+
+````
+
---
+++ src/js/components/select-dropdown.js
@@ -0,0 +1,177 @@
+// A control that mimics the ordinary <select>, but in an more cross-browser look-n-feel.
+// It is built on Bootstrap's (react-bootstrap) dropdown button/menu funtionality.
+
+// Example:
+//
+//  <Select
+//    id={'dropdown-timespan'}
+//    name={'timespan'}
+//    value={this.props.timespan}
+//    onChange={(val) => (this.props.setTimespan(val))}
+//   >
+//    <option key={'day'} value={'day'}>{'Day'}</option>
+//    <option key={'month'} value={'month'}>{'Month'}</option>
+//    <option key={'year'} value={'year'}>{'Year'}</option>
+//  </Select>
+
+var _ = global.lodash || require('lodash');
+var React = global.React || require('react');
+var ReactBootstrap = global.ReactBootstrap || require('react-bootstrap');
+var {Dropdown, MenuItem} = ReactBootstrap;
+
+var PropTypes = React.PropTypes;
+
+var isSameMap = function (map1, map2)
+{
+  // We consider 2 maps equal if they contain the same (k,v) pairs with
+  // exactly the same order
+  var a1 = Array.from(map1.entries()), a2 = Array.from(map2.entries());
+  return _.zip(a1, a2).every(p => (_.isEqual(...p)));
+}
+
+var Select = React.createClass({
+  
+  mixins: [
+    React.addons.pureRenderMixin,
+  ],
+
+  propTypes: {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    options: PropTypes.instanceOf(Map), // if supplied, has precedence over children <option>s
+    // Appearence
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    // Callbacks
+    onSelect: PropTypes.func,
+    onChange: PropTypes.func,
+  },
+
+  getInitialState: function () {
+    var options = this.constructor.makeOptionMap(this.props);
+    var value = this.props.value;
+    return {
+      id: this.props.id,
+      value: (value && options.has(value))? value : null,
+      options: options,
+    };
+  },
+  
+  getDefaultProps: function () {
+    return {
+      onSelect: (val) => (console.info('Selected: ' + val)),
+      width: '5em',
+    };
+  },
+  
+  componentWillReceiveProps: function (nextProps) {
+    if (nextProps.id != this.props.id) {
+      throw new Error('The `id` property is not supposed to be updated');
+    }
+    
+    var updated = {};
+    
+    var nextOptions = this.constructor.makeOptionMap(nextProps);
+    if (!isSameMap(this.state.options, nextOptions)) {
+      updated.options = nextOptions;
+    }
+    
+    var value = nextProps.value;
+    if (value && nextOptions.has(value)) {
+      updated.value = value;
+    }
+
+    if (!_.isEmpty(updated)) {
+      this.setState(updated);
+    }
+  },
+
+  render: function () {
+    var options = this.state.options; 
+    var value = this.state.value;
+    
+    var textprops = 
+    {
+      className: 'title text',
+      style: {
+        width: this.props.width,
+        display: 'inline-block',
+        verticalAlign: 'top',
+        textAlign: 'left',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      }
+    };
+    
+    var text = value? (options.get(value)) : (this.props.placeholder || '');
+     
+    // Maintain a controlled <input> in order to be compatible with an ordinary forms
+    var input = (this.props.name)? 
+      (<input type="hidden" name={this.props.name} value={this.props.value || ''}/>) : null;
+    
+    var itemBuilder = (val) => (
+      <MenuItem key={val} eventKey={val} value={val}>{options.get(val)}</MenuItem>
+    );
+    
+    return (
+      <Dropdown 
+        id={this.state.id}
+        onSelect={(ev, val) => (this._handleSelection(val))} 
+       >
+        {input}
+        <Dropdown.Toggle>
+          <span {...textprops}>{text}</span>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {Array.from(options.keys()).map(itemBuilder)}
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  },
+
+  // Callbacks
+  
+  _handleSelection: function (val) {
+    
+    // Fire the supplied callbacks
+
+    if (_.isFunction(this.props.onSelect)) {
+      this.props.onSelect.call(undefined, val);
+    }
+    
+    if (_.isFunction(this.props.onChange) && val != this.state.value) {
+      this.props.onChange.call(undefined, val);
+    }
+
+    return false;
+  },
+
+  // Helpers
+  
+  statics: {
+    
+    makeOptionMap: function (props) {
+      if (props.options) { 
+        // No need to convert anything
+        return props.options;
+      }
+      
+      var children = props.children;
+      if (children == null)
+        return new Map([]);
+      
+      console.assert(
+        children.every(c => (c.type == 'option')),
+        'Expected all children to be plain <option> elements!'
+      );
+
+      return new Map(children.map((c) => (
+        [c.props.value, c.props.children.toString()]
+      )));
+    },
+  },
+
+});
+
+module.exports = Select;
